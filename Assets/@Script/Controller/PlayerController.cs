0cd943dcd0a2624c1ce4b3995ffd91398205bbda
kimgapryong <kimjagium@gmail.com>
2025-07-22 09:58:26 +0900

    Player

--- Assets/@Script/Controller/PlayerController.cs
+++ Assets/@Script/Controller/PlayerController.cs
@@ -4,5 +4,24 @@ using UnityEngine;
 
 public class PlayerController : CreatureController
 {
-   
+    protected override void Update()
+    {
+        PlayerWalk();
+        base.Update();
+    }
+
+    private void PlayerWalk()
+    {
+        float x = Input.GetAxisRaw("Horizontal") * Speed;
+        float z = Input.GetAxisRaw("Vertical") * Speed;
+
+        if (x == 0 && z == 0)
+        {
+            rb.velocity = Vector3.Lerp(rb.velocity, Vector3.zero, 0.6f * Time.deltaTime);
+            return;
+        }
+            
+        rb.velocity = new Vector3(x, rb.velocity.y, z);
+
+    }
 }
